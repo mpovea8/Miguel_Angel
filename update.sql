d2457201b5024428ec2837ee65c21a73a951f419
Angela Casa <amorort595a@ieslacampina.es>
2020-11-10 20:35:18 +0100

    arreglo de todas las sentencias

--- update.sql
+++ update.sql
@@ -1,3 +1,3 @@
 update cursos set nombre = "IAW" where nombre like "ASIR";
-UPDATE AULA SET nombre ='Musica' WHERE nombre like 'Quimica';
+UPDATE AULAS SET nombre ='Musica' WHERE nombre like 'Quimica';
 update cursos set nombre ='curso' where nombre like '1º';
